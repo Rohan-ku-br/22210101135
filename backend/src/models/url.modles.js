372666e0c1b69b55abf63f0cc630d15589b80936
Rohan Kumar <rohanbarnwal108@gmail.com>
2025-09-08 13:19:26 +0530

    second commit

--- backend/src/models/url.modles.js
+++ backend/src/models/url.modles.js
@@ -1,13 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
+const clickSchema = new mongoose.Schema({
+  time: { type: Date, default: Date.now },
+  referrer: String,
+  location: String,
+});
 
 const urlSchema = new mongoose.Schema({
-    originalUrl: { type: String, required: true },
+  originalUrl: { type: String, required: true },
   shortcode: { type: String, unique: true },
   createdAt: { type: Date, default: Date.now },
   expiry: { type: Date },
-  // clicks: [clickSchema],
-})
+  clicks: [clickSchema],
+});
 
-const urlModel = mongoose.model('url', urlSchema)
+const urlModel = mongoose.model('url', urlSchema);
 
-module.exports = urlModel
+module.exports = urlModel;
--- backend/src/routes/url.Routes.js
+++ backend/src/routes/url.Routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { createShortURL, redirect, getStats } = require('../controller/urlController');
 
 router.post('/shorturls', createShortURL);
-router.get('/shorturls/shortcode', getStats);
-router.get('/shortcode', redirect);
+router.get('/shorturls/:shortcode', getStats);   
+router.get('/:shortcode', redirect);             
 
 module.exports = router;
